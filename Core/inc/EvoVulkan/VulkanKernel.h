9285d94ef11e7542a8a6f9fc8e37c7dd801d4d7c
Monika0000 <rotaru5craft@gmail.com>
2022-09-13 12:11:17 +1000

    Фикс краша на видеокартах NVIDIA

--- Core/inc/EvoVulkan/VulkanKernel.h
+++ Core/inc/EvoVulkan/VulkanKernel.h
@@ -80,7 +80,7 @@ namespace EvoVulkan::Core {
         void SetMultisampling(uint32_t sampleCount);
         void SetSwapchainImagesCount(uint32_t count);
 
-        void SetGUIEnabled(bool enabled);
+         virtual void SetGUIEnabled(bool enabled);
 
         bool SetValidationLayersEnabled(bool value);
         void SetSize(uint32_t width, uint32_t height);
